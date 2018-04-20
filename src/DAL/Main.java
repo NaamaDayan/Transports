572949f3367f19e28cb706a1a4791ecc1f82ae98
Unknown <namadayan@gmail.com>
2018-04-20 12:05:59 +0300

    add tables - foreign key does not work yet

--- src/DAL/Main.java
+++ src/DAL/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String args[]) {
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:transports.db");) {
             Class.forName("org.sqlite.JDBC");
-
             createTable(conn);
 
             conn.close();
@@ -19,17 +18,116 @@ public class Main {
 
     }
 
-
-    private static void createTable(Connection conn) {
+    private static void createTable(Connection conn){
+        createTracksTable(conn);
+        createDriversTable(conn);
+        createLicensesTable(conn);
+        createDeliveriesTable(conn);
+        createPlacesTable(conn);
+        createDeliveryDestinationsTable(conn);
+        createLicensesForDriversTable(conn);
+    }
+    private static void createTracksTable(Connection conn) {
         try (Statement stmt = conn.createStatement();) {
+            //create tracks
             String sql = "CREATE TABLE Tracks " +
+                    "(ID VARCHAR(9) PRIMARY KEY NOT NULL," +
+                    " MODEL           TEXT    NOT NULL, " +
+                    " COLOR           TEXT    NOT NULL, " +
+                    " NETO_WEIGHT         INT, " +
+                    "MAX_WEIGHT         INT)";
+            stmt.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void createDriversTable(Connection conn) {
+        try (Statement stmt = conn.createStatement();) {
+
+            String sql = "CREATE TABLE Drivers " +
                     "(ID VARCHAR(9) PRIMARY KEY     NOT NULL," +
-                    " FIRST NAME           TEXT    NOT NULL, " +
-                    " LAST NAME           TEXT    NOT NULL, " +
-                    " SALARY         INT)";
+                    " FIRST_NAME           TEXT    NOT NULL, " +
+                    " LAST_NAME           TEXT    NOT NULL, " +
+                    "PHONE_NUMBER         TEXT)";
             stmt.executeUpdate(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+    private static void createLicensesTable(Connection conn) {
+        try (Statement stmt = conn.createStatement();) {
+
+            String sql = "CREATE TABLE Licenses " +
+                    "(LICENSE_TYPE VARCHAR (9) NOT NULL," +
+                    " TRACK_MODEL  TEXT FOREIGN KEY REFERENCES Tracks(MODEL)" +
+                    "PRIMARY KEY (LICENSE_TYPE, TRACK_MODEL))";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createLicensesForDriversTable(Connection conn) {
+        try (Statement stmt = conn.createStatement();) {
+
+            String sql = "CREATE TABLE LicensesForDrivers " +
+                    "(DRIVER_ID VARCHAR (9) FOREIGN KEY REFERENCES Drivers(ID)," +
+                    "LICENSE_TYPE VARCHAR (9) FOREIGN KEY REFERENCES Licenses(LICENSE_TYPE)," +
+                    "PRIMARY KEY (LICENSE_TYPE, DRIVER_ID))";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createPlacesTable(Connection conn) {
+        try (Statement stmt = conn.createStatement();) {
+
+            String sql = "CREATE TABLE Places " +
+                    "(PLACE_ID VARCHAR (9) PRIMARY KEY NOT NULL," +
+                    "ADDRESS TEXT NOT NULL ," +
+                    "PHONE_NUMBER TEXT, " +
+                    "CONTACT_NAME TEXT)";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createDeliveriesTable(Connection conn) {
+        try (Statement stmt = conn.createStatement();) {
+
+            String sql = "CREATE TABLE Deliveries " +
+                    "(DELIVERY_ID VARCHAR (9) PRIMARY KEY NOT NULL," +
+                    "DATE TEXT NOT NULL, " +
+                    "LEAVING_HOUR TEXT NOT NULL ," +
+                    "TRACK_ID VARCHAR (9) FOREIGN KEY REFERENCES Tracks(ID)," +
+                    "DRIVER_ID VARCHAR (9) FOREIGN KEY REFERENCES Drivers(ID)," +
+                    "ORDER_NUMBER VARCHAR (9) NOT NULL, " +
+                    "SOURCE_ID VARCHAR(9) FOREIGN KEY REFERENCES Places(PLACE_ID))";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private static void createDeliveryDestinationsTable(Connection conn) {
+        try (Statement stmt = conn.createStatement();) {
+            String sql = "CREATE TABLE DeliveryDestinations " +
+                    "(DELIVERY_ID VARCHAR (9), FOREIGN KEY (DELIVERY_ID) REFERENCES Deliveries(DELIVERY_ID)," +
+                    "PLACE_ID VARCHAR (9), FOREIGN KEY (PLACE_ID) REFERENCES Places(PLACE_ID)," +
+                    "PRIMARY  KEY(DELIVERY_ID, PLACE_ID)";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
