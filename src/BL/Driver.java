eedab0be4fe47e003ddd45bdac2894606ae4adae
Unknown <namadayan@gmail.com>
2018-04-21 06:02:09 +0300

    Add classes

--- src/BL/Driver.java
+++ src/BL/Driver.java
@@ -4,7 +4,20 @@ package BL;
 import DAL.Drivers;
 
 public class Driver{
-        public static void insertDriver(String id, String first, String last, String phone) {
-            Drivers.insertDriver(id, first, last, phone);
-        }
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Driver(String id, String firstName, String lastName, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void insertDriver() {
+        Drivers.insertDriver(id, firstName, lastName, phoneNumber);
+    }
 }
--- src/BL/Truck.java
+++ src/BL/Truck.java
@@ -4,7 +4,21 @@ import DAL.Trucks;
 
 public class Truck {
 
-public static void insertTruck(String id, String model, String color, int netoWeight, int maxWeight) {
+    private String id;
+    private String model;
+    private String color;
+    private int netoWeight;
+    private int maxWeight;
+
+    public Truck(String id, String model, String color, int netoWeight, int maxWeight) {
+        this.id = id;
+        this.model = model;
+        this.color = color;
+        this.netoWeight = netoWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public void insertTruck() {
     Trucks.insertTruck(id, model,color, netoWeight, maxWeight );
 }
 
