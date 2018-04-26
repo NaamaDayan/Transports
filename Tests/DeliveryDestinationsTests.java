00987e7e1c8b6597f99459ce0037f9d851d54c9f
Shahar Hoory <shaharhoo@gmail.com>
2018-04-26 18:15:01 +0300

    Auto stash before merge of "master" and "origin/master"

--- Tests/DeliveryDestinationsTests.java
+++ Tests/DeliveryDestinationsTests.java
@@ -1,3 +1,4 @@
+import BL.Entities.Order;
 import BL.Entities.Truck;
 import BL.Entities.TruckModel;
 import DAL.*;
@@ -10,6 +11,8 @@ import java.sql.Date;
 import java.sql.Time;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Naama on 23/04/2018.
@@ -31,12 +34,15 @@ public class DeliveryDestinationsTests {
         Date date = new java.sql.Date(format.parse("12.12.2012", new ParsePosition(0)).getTime());
         java.sql.Time hour = new java.sql.Time(new Time(hourformat.parse("14:12").getTime()).getTime());
         TruckModel m1 = new TruckModel("1", "bla");
+        Models.insertModel(m1);
         Truck t = new Truck("12", m1, "red", 123, 350);
         Trucks.insertTruck(t);
         Drivers.insertDriver("12", "roni" ,"shtern", "0502242");
         Places.insertPlace("12", "yerushalmi", "03053", "noy");
         Deliveries.insertDeivery("123",date, hour, "12", "12", "12");
-        DeliveryDestinations.insertDeliveryDestination("123", "12", null);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("toy", 2);
+        DeliveryDestinations.insertDeliveryDestination("123", "12", new Order("11", map ));
         Assert.assertTrue(DeliveryDestinations.isDestExistInDelivery("123", "12"));
         Assert.assertFalse(DeliveryDestinations.isDestExistInDelivery("123", "16"));
 
